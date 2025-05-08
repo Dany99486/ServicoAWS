33184e7f13fcdb42b0096db0a8999ce0fd51c62e
Danie <daniel8bravo@gmail.com>
2025-05-08 12:14:37 +0100

    Update settings.py

--- ServicoAWS/settings.py
+++ ServicoAWS/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'servicos',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -124,13 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
-
-CORS_ALLOW_ALL_ORIGINS = True
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
